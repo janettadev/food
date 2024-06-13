2774829909134a861431aed0969801193492a856
FatimaDEV <shamudinovafatima@gmail.com>
2024-06-13 19:40:00 +0600

    create product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import CreateProduct from "./product/CreateProduct";
 
 function App() {
   return (
     <div className="App">
+      <CreateProduct />
     </div>
   );
 }
